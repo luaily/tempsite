498246eb6e896a2c61ad6db3e73e0958b2b8a2e6
luaily <113217866+luaily@users.noreply.github.com>
2026-02-03 23:41:20 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 1. Tell Next.js to generate a static site (creates the ./out directory)
+  output: 'export',
+  
+  // 2. Disable server-based image optimization (required for static export)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
